5c7a72aa724ca1ea2ff08bac1559b5a07cba3670
Summer 19 <summer19@WL-218-97.WPA.Claremont.Edu>
2019-06-11 14:05:47 -0700

    added second is k anonymous

--- hw4.py
+++ hw4.py
@@ -4,12 +4,13 @@ import utils.tools as tools
 
 
 '''
-Question1: Use pandas to read data newAdult.csv. How many rows are there in this data set? 
+Question1: Use pandas to read data newAdult.csv, a data set from 1994 Census database (https://archive.ics.uci.edu/ml/datasets/adult).
+            How many rows are there in this data set? 
             What are the column names? 
             Is there any personal Identifiable information in the data set?
             Is there any quasi-identifier information according to Sweeney's defination?
             Can you find possible quasi-identifier among the columns? Why are they?
-            How unique the data is with age, fnlwgt, native_country being quasi-identifier?
+            How many unique the data can be identified with age, fnlwgt, native_country being quasi-identifier?
 '''
 
 f = pd.read_csv('newAdult.csv')
@@ -36,10 +37,10 @@ age, fnlwgt, native_country. because value_counts have 1, so more likely to id p
 '''
 
 '''
-Question 2: Suppose now you have a sensitive dataframe of 10,000 rows, link_attack.csv,
-            which contains personal identifiable information, perform a link attack
+Question 2: Suppose now you have a sensitive dataframe of 10,000 rows named as "link_attack.csv",
+            which contains personal identifiable information. Perform a link attack
             to re-identify people's name, ssn, race, and salary. You can use ['age',  'fnlwgt',
-            'native_country']
+            'native_country'] as quasi-identifier. How many people can you identify?
     
 '''
 link_attack = pd.read_csv('link_attack.csv')
@@ -54,13 +55,34 @@ merged = pd.merge(f, link_attack, on = ['age','fnlwgt', 'native_country'])
 
 '''
 Question 3: Consider the definiaiton of k-anonymity, write a function to decide if 
-            a data set is k-anonymous for a list of quasi-identifier
+            a data set is k-anonymous for a list of quasi-identifier. Then test if 
+            this data set adult.csv is 2 anonymous with ['age', 'sex','native_country']
+            being quasi-identifiers
 '''
-qsi = ['age',  'fnlwgt',
-        'sex',
-       'native_country',
-    ]
+
 def is_k_anonymous(k, df, qsi):
+    '''
+    k: a constant to descide if a data set is k anonymous
+    df: the data set of interest
+    qsi: a list of quasi-identifier
+    return: True if df is k-anonymous, False if not
+    '''
+    df_qsi = df.loc[:3,qsi]
+    # print(df_qsi)
+    count = 0
+    for index, row in df_qsi.iterrows():
+        for index_c, compare_row in df_qsi.iterrows():
+            # print(compare_row)
+            if row.equals(compare_row):
+                count += 1
+                if count >= k:
+                    break
+                
+        if count < k:
+            return False
+    return True
+
+def is_k_anonymous2(k, df, qsi):
     '''
     k: a constant to descide if a data set is k anonymous
     df: the data set of interest
@@ -69,18 +91,32 @@ def is_k_anonymous(k, df, qsi):
     '''
     df_qsi = df.loc[:,qsi]
     # print(df_qsi)
-    count = 0
-    for index, row in df_qsi.iterrows():
-        for index_c, compare_row in df_qsi.iterrows():
-            if row.equals(compare_row):
-                count += 1
-                if count >= k:
-                    break
-        if count < k:
-            return False
-    return True
+    sythesized = df_qsi[qsi[0]].map(str) # + anon['Zip'].map(str)
+    for i in range(1,len(qsi)):
+        sythesized += df_qsi[qsi[i]].map(str) 
+    # sythesized.map(str)
+    # dup = df_qsi[df_qsi.duplicated(keep = False)] 
+    count = sythesized.value_counts(ascending = True)
+    if count[0] < k:
+        return False
+    else:
+        return True
+    # print(count[0] )
 
-print(is_k_anonymous(2,f.head(2), qsi))
+qsi = ['age', 'sex', 'native_country']
+# print(is_k_anonymous2(2,f, qsi))
+
 '''
-this data set is not k anonymous with this quasi-identifers
+this data set is not 2 anonymous with this quasi-identifers
 '''
+
+'''
+Question 4: Now we wish to generalize the informations in adult.csv to make it k-anonymous while minimising
+            the lose of information as little as possible. This problem turns out to be NP-Hard, which means 
+            there is no polynomial algorithm to solve it. However, a compariably fast algorithm can solve it
+            sub-optimal. The detail of this algorithm can be found at: https://github.com/qiyuangong/Mondrian
+            Read through it, and use this package to make this data set 2-anonymous.
+'''
+import os
+cmd = 'python anonymizer.py s "adult.csv" 2'
+# anony = pd.read_csv()
--- readme.md
+++
@@ -1,3 +0,0 @@
-# Differential Privacy
-
-## Link attack
