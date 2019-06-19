cee9681be5a0cae1268b204caf3ae82bf0e49006
Summer 19 <summer19@WL-218-97.WPA.Claremont.Edu>
2019-06-19 09:57:44 -0700

    modified student version

--- k_anony/students/hw4.py
+++ k_anony/students/hw4.py
@@ -16,14 +16,15 @@ Question1:  Use pandas to read data adult.data, a data set from 1994 Census data
             Can you find possible quasi-identifier among the columns? Why are they?
 '''
 
-# TODO: Read data\adult.data and add columns to the data set. How many rows are there in this data set? 
+# TODO: Read "data\adult.data" and add columns to the data set. How many rows are there in this data set? 
 
 
 
 
 # TODO: Is there any personal Identifiable information in the data set?
 #       Is there any quasi-identifier information according to Sweeney's defination?
-#       Can you find possible quasi-identifier among the columns? Why are they?
+#       Can you find possible quasi-identifier among the columns? 
+#       Why do you think they can be quasi-identifier?
 
 
 
@@ -32,6 +33,8 @@ Question 2: Your supervisor believes the information in ['fnlwgt', 'education', 
             'capital_gain', 'capital_loss', 'hours_per_week'] are not necessary for your analysis.
             Use suppression technique to de-idenitify this data set. How many rows are unique in 
             the suppressed data set? 
+            Hint: Suppression technique and other de-identify techniques can be found in the reading 
+            list slides https://www2.cs.duke.edu/courses/fall12/compsci590.3/slides/lec3.pdf
 '''
 # TODO: Use suppression technique to de-idenitify this data set. 
 #       Then save the new data set under data directory as 'suppressed.csv' without a header.
@@ -43,10 +46,11 @@ Question 2: Your supervisor believes the information in ['fnlwgt', 'education', 
 
 
 '''
-Question 3: Suppose now you have a sensitive dataframe of 10,000 rows named as "link_attack.csv",
+Question 3: Suppose now you have a sensitive data set of 10,000 rows named as "link_attack.csv",
             which contains personal identifiable information. Take a look at the head of this attack data,
-            perform a link attack and to re-identify people's name, ssn, race, and salary. You can use 
-            ['age',  'sex, 'native_country'] as quasi-identifier. How many people can you identify?
+            perform a link attack and to re-identify people's name, ssn, race, and income. 
+            Hint: You can use ['age',  'sex, 'native_country'] as quasi-identifier. 
+            How many people can you identify?
  
 '''
 # TODO: Perform link attack and identify individuals. How many of them can you idenity?
@@ -75,17 +79,21 @@ def is_k_anonymous(k, df, qsi):
 Question 5: Now we wish to generalize the informations in adult.csv to make it k-anonymous while minimising
             the lose of information as little as possible. This problem turns out to be NP-Hard, which means 
             there is no polynomial algorithm to solve it. However, a compariably fast algorithm can solve it
-            sub-optimal. The detail of this algorithm can be found at: https://github.com/qiyuangong/Mondrian
+            sub-optimal. 
+            This algorithm is algready implemented for you. 
+            The detail of this algorithm can be found at: https://github.com/qiyuangong/Mondrian
             Read through it, and use this package to make this data set 2-anonymous.
 '''
 
-# TODO: Use os module to run command 'python3 anonymizer.py s a 2'. Read the head generated data 'data/anonymized.csv'
-#       then add column names ['age', 'workclass',  'education_num','marital_status', 'moving', 'race', 'sex','native_country','income']
+# TODO: Use os module to run command 'python3 anonymizer.py s a 2'. Read the head generated data '
+#       data/anonymized.csv' then add column names ['age', 'workclass',  'education_num','marital_status',
+#       'moving', 'race', 'sex','native_country','income']
 #       back to the anonymized data set. Observe the data by printing out the head and describe funtion.
 
 
 
-# TODO: Test if it is k-anonymous using the function in Question 3 and same quasi-identifier. How many rows in this data set are unique now?
+# TODO: Test if it is k-anonymous using the function in Question 3 and same quasi-identifier. 
+#       How many rows in this data set are unique now?
 
 
 
