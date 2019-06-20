cb2a773c8998ad311c4fe0f5b85d825845a877e4
Summer 19 <summer19@WL-218-97.WPA.Claremont.Edu>
2019-06-20 14:49:15 -0700

    added median null

--- data_vis/instructor/hw1.py
+++ data_vis/instructor/hw1.py
@@ -22,11 +22,28 @@ df = pd.read_csv(PATH)
 
 df['age_group'] = pd.cut(df['Age'],[0,10,20,30,40,50,60,70,80]).astype(str)
 age_groups = df.groupby('age_group').sum()
-print(age_groups)
+# print(age_groups)
 age_group_count = df['age_group'].value_counts()
-print(age_group_count)
+# print(age_group_count)
 survive_rate = age_groups['Survived'] / age_group_count
-print(survive_rate)
-sns.barplot(x= survive_rate.index, y = survive_rate.values)
-plt.show(sns)
+# print(survive_rate)
+# sns.barplot(x= survive_rate.index, y = survive_rate.values)
+# sns.pointplot(x='Sex', y='Survived', hue='Pclass', data=df)
+# plt.show(sns)
 
+# df.dropna(axis = 1,  inplace = True)
+print(re.findall("\w+[.]", 'Heikkinen, Miss. Laina'))
+# print(df['Age'].astype(str)[0][0])
+
+# 5.b
+df.dropna(subset = ['Age'], inplace = True)
+# print( df )
+dfS = df.groupby('Survived').median()
+# dfS.plot(kind='pie', y = 'Age',autopct='%1.1f%%')
+# df['MedianAge'] = df.groupby('Survived')['Age'].transform("median")
+# df.loc[0:100,['MedianAge']] = 100
+dfS.rename(columns = {'Age': 'MedianAge'}, inplace = True)
+print(dfS.head())
+# sns.barplot(x = dfS.index, y = dfS.Age)
+plt.show(sns)
+# print(dfS)
