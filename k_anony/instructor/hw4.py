4fefc93e3840d701f795e5fd67c562ecbfed283d
Summer 19 <summer19@WL-218-97.WPA.Claremont.Edu>
2019-06-21 15:22:45 -0700

    finished hw4 test cases

--- k_anony/instructor/hw4.py
+++ k_anony/instructor/hw4.py
@@ -1,38 +1,51 @@
+""" 
+Author: Yun Zhang
+Class: CS 181R
+Week 7 - Data Privacy and Anonymity
+Homework 4
+Name:
+"""
 import pandas as pd
 import numpy as np
-# import utils.tools as tools 
+import os
 
+df = pd.read_csv('data/adult.data')
 
 '''
-Question1:  Use pandas to read data newAdult.csv, a data set from 1994 Census database (retrieved
-            from: https://archive.ics.uci.edu/ml/datasets/adult).
-            How many rows are there in this data set? 
-            What are the column names? 
+Question1:  Use pandas to explore the data set from 1994 Census database. 
+            How many rows are there in this data set?  
+            Add column names to this data set. The list of column names are: ['age', 'workclass', 
+            'fnlwgt', 'education', 'education_num', 'marital_status', 'moving', 'relationship', 
+            'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country','income']
             Is there any personal Identifiable information in the data set?
             Is there any quasi-identifier information according to Sweeney's defination?
             Can you find possible quasi-identifier among the columns? Why are they?
+
+            Write some codes after TODO. Then write your response in a seperate file and save it as hw4.pdf.
 '''
 
-f = pd.read_csv('data/adult.data')
-f.columns = ['age', 'workclass', 'fnlwgt', 'education', 'education_num',
+# TODO: Add column names to df. How many rows are there in this data set? 
+df.columns = ['age', 'workclass', 'fnlwgt', 'education', 'education_num',
 'marital_status', 'moving', 'relationship', 'race', 'sex',
 'capital_gain','capital_loss', 'hours_per_week', 'native_country','income']
-# TODO: How many rows are there in this data set? What are the column names? 
 
-print(f.describe())
+print('there are', df.shape[0], 'rows in df')
 
 '''
 there are 32560 rows in the data set
-
 '''
 
-# TODO: Is there any personal Identifiable information in the data set?
-#       Is there any quasi-identifier information according to Sweeney's defination?
-#       Can you find possible quasi-identifier among the columns? Why are they?
+# TODO      : Is there any personal Identifiable information in the data set?
+#           Is there any quasi-identifier information according to Sweeney's defination?
+#           Can you find possible quasi-identifier among the columns? 
+#           Why do you think they can be quasi-identifier?
+#           Hint: use value_counts() to observe how unique a values is for each column. Column with more unique
+#               values are more likely to be used as quasi-identifier.
 
-print(f.age.value_counts())
-print(f.fnlwgt.value_counts())
-print(f.native_country.value_counts())
+
+# print(f.age.value_counts())
+# print(f.fnlwgt.value_counts())
+# print(f.native_country.value_counts())
 '''
 there is no pii : https://piwik.pro/blog/what-is-pii-personal-data/
 sex is quasi-identifier in Sweeny's defination
@@ -42,23 +55,23 @@ age, fnlwgt, native_country. because value_counts have 1, so more likely to id p
 '''
 Question 2: Your supervisor believes the information in ['fnlwgt', 'education', 'relationship', 
             'capital_gain', 'capital_loss', 'hours_per_week'] are not necessary for your analysis.
-            Use suppression technique to de-idenitify this data set. How many rows are unique in 
-            the suppressed data set? 
-            Helpful pandas methods: http://pandas.pydata.org/pandas-docs/version/0.17/generated/pandas.DataFrame.drop_duplicates.html
+            Use suppression technique to de-idenitify this data set. Name the new data frame 
+            variable 'suppressed' and save it as 'suppressed.csv' without a header, without index. 
+            How many rows are unique in the 'suppressed' data set? 
 '''
 # TODO: Use suppression technique to de-idenitify this data set. 
-#       Then save the new data set under data directory as 'suppressed.csv' without a header.
+#       Then save the new data set under data directory as 'suppressed.csv' without a header, without index.
 
-suppressed = f.drop(columns = ['fnlwgt', 'education', 'relationship', 
+suppressed = df.drop(columns = ['fnlwgt', 'education', 'relationship', 
                             'capital_gain', 'capital_loss', 'hours_per_week'])
-print(suppressed.head())
+
 suppressed.to_csv('data/suppressed.csv', header = False, index = False)
 
 
 # TODO: How many rows in suppressed data set are unique?
 unique = suppressed.drop_duplicates(keep = False)
-print(unique.head()) 
-print(unique.describe()) 
+# print(unique.head()) 
+# print(unique.describe()) 
 '''
 17047 unique
 '''
@@ -71,12 +84,13 @@ Question 3: Suppose now you have a sensitive dataframe of 10,000 rows named as "
             ['age',  'sex, 'native_country'] as quasi-identifier. How many people can you identify?
  
 '''
+# TODO: your code here
 link_attack = pd.read_csv('data/link_attack.csv')
 merged = pd.merge(suppressed, link_attack, on = ['age','sex', 'native_country'])
 
 merged_unique = merged.drop_duplicates(['age','sex', 'native_country'], keep = False)
-print(merged_unique.head())
-print(merged_unique.describe()) 
+# print(merged_unique.head())
+# print(merged_unique.describe()) 
 '''
 287 people can be identified by link attack
 '''
@@ -133,7 +147,7 @@ def is_k_anonymous2(k, df, qsi):
     # print(count[0] )
 
 qsi = ['age', 'sex', 'native_country']
-print(is_k_anonymous(2, suppressed, qsi))
+# print(is_k_anonymous(2, suppressed, qsi))
 
 '''
 this data set is not 2 anonymous with this quasi-identifers
@@ -146,32 +160,35 @@ Question 5: Now we wish to generalize the informations in adult.csv to make it k
             sub-optimal. The detail of this algorithm can be found at: https://github.com/qiyuangong/Mondrian
             Read through it, and use this package to make this data set 2-anonymous.
 '''
-
-# f.columns = [''] * len(f.columns)
-# f.to_csv(r'data/newAdult4.csv',index=False, header = False)
-
-# TODO: Use os module to run command 'python3 anonymizer.py s a 2'. Read the head generated data 'data/anonymized.csv'
-#       then add column names ['age', 'workclass',  'education_num','marital_status', 'moving', 'race', 'sex','native_country','income']
-#       back to the anonymized data set. Observe the data by printing out the head and describe funtion.
-import os
 cmd = 'python3 anonymizer.py s a 2'
+
+# TODO: Use os module to run cmd command. 
+#       then add column names 
+#       back to the anonymized data set. Observe the data by printing out the head and describe funtion.
+
 os.system(cmd) 
+
+# TODO: Read the anonymized data generated by this Mondrian implementation and save it in anony variable.
+#        Then add column names ['age', 'workclass',  'education_num','marital_status', 'moving', 'race', 
+#       'sex','native_country','income'] and observe this data set.
 anony = pd.read_csv('data/anonymized.csv')
 anony.columns = ['age', 'workclass',  'education_num',
        'marital_status', 'moving', 'race', 'sex',
         'native_country','income']
-print('k-anonymized data !!!!')
-print(anony.head())
-print(anony.describe())
+# print('k-anonymized data !!!!')
+# print(anony.head())
+# print(anony.describe())
 
 
-# TODO: Test if it is k-anonymous using the function in Question 3 and same quasi-identifier. How many rows in this data set are unique now?
+# TODO: Test if it is k-anonymous using the function in Question 3 and same quasi-identifier. 
+#       How many rows in this data set are unique now? We can call the non-duplicate data set
+#       as unique anony.
 
-print(is_k_anonymous(2,anony, qsi))
+# print(is_k_anonymous(2,anony, qsi))
 
 unique_anony = anony.drop_duplicates(['age', 'sex', 'native_country'], keep = False)
-print(unique_anony.head()) #
-print(unique_anony.describe()) #
+# print(unique_anony.head()) #
+# print(unique_anony.describe()) #
 
 '''
 It is 2-anonymous now. And there are 0 unique rows with qsi = ['age', 'sex', 'native_country']
@@ -181,7 +198,7 @@ It is 2-anonymous now. And there are 0 unique rows with qsi = ['age', 'sex', 'na
 # print(monAnony.describe())
 '''
 Question 6: Use the anonymized data set, analyze what percentage of male and female has income 
-            less than 50k? Compare the result with the original datase 'newAdult.csv'.
+            less than 50k? Compare the result with the original datase 'adult.data'.
 '''
 sex_group = anony.groupby(['sex'])
 for sex, sex_df in sex_group:
@@ -191,13 +208,55 @@ for sex, sex_df in sex_group:
 # male 13685/20017, female 8002/9062
 
 print('original data!!!!!!!')
-sex_group2 = f.groupby(['sex'])
+sex_group2 = df.groupby(['sex'])
 for sex, sex_df in sex_group:
     print(sex)
     print(sex_df.describe())
 
 # male 13685/20017, female 8002/9062
 
+def main():
+    # test 1
+    assert np.array_equal(df.columns, ['age', 'workclass', 
+            'fnlwgt', 'education', 'education_num', 'marital_status', 'moving', 'relationship', 
+            'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country','income']) == True
+    # test 2
+    assert np.array_equal(suppressed.columns, ['age', 'workclass', 'education_num',
+            'marital_status', 'moving', 'race', 'sex', 'native_country','income'] ) == True
+
+    assert unique.shape[0] == 17047
+
+    # test 3
+    assert merged_unique.shape[0] == 287
+
+    # test 4
+    assert is_k_anonymous(2, suppressed, qsi) == False
+
+    two_anon = pd.DataFrame(
+    [
+        ['*', 'Storm',34,'Black'],
+        ['John','*','*','*'],
+        ['*', 'Storm',34,'Black'],
+        ['John','*','*','*']
+    ],
+    columns = ['First','Last','age','race']
+)
+    assert is_k_anonymous(2, two_anon, ['First','Last','age','race']) == True
+
+    # test 5
+    assert is_k_anonymous(2,anony, qsi) == True
+    assert unique_anony.shape[0] == 0
+
+    # test 6
+    # print(df.groupby('sex').get_group(' Male')['income'].value_counts().tolist())
+    # print(df.groupby('sex').groups.keys())
+    # assert anony.groupby('sex').get_group('Male')['income'].value_counts().tolist()[0] == 13685
+    # assert df.groupby('sex').get_group(' Male')['income'].value_counts().tolist()[0] == 13685
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
--- k_anony/students/hw4.py
+++ k_anony/students/hw4.py
@@ -1,75 +1,89 @@
+""" 
+Class: CS 181R
+Week 7 - Data Privacy and Anonymity
+Homework 4
+Name:
+"""
 import pandas as pd
 import numpy as np
 import os
 
+df = pd.read_csv('data/adult.data')
+
 '''
-Question1:  Use pandas to read data adult.data, a data set from 1994 Census database (retrieved
-            from: https://archive.ics.uci.edu/ml/datasets/adult).
+Question1:  Use pandas to explore the data set from 1994 Census database. 
+            How many rows are there in this data set?  
             Add column names to this data set. The list of column names are: ['age', 'workclass', 
             'fnlwgt', 'education', 'education_num', 'marital_status', 'moving', 'relationship', 
             'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country','income']
-            How many rows are there in this data set? 
             Is there any personal Identifiable information in the data set?
             Is there any quasi-identifier information according to Sweeney's defination?
             Can you find possible quasi-identifier among the columns? Why are they?
+
+            Write some codes after TODO. Then write your response in a seperate file and save it as hw4.pdf.
 '''
 
-# TODO: Read "data\adult.data" and add columns to the data set. How many rows are there in this data set? 
+# TODO: Add column names to df. How many rows are there in this data set? 
 
 
+# TODO      : Is there any personal Identifiable information in the data set?
+#           Is there any quasi-identifier information according to Sweeney's defination?
+#           Can you find possible quasi-identifier among the columns? 
+#           Why do you think they can be quasi-identifier?
+#           Hint: use value_counts() to observe how unique a values is for each column. Column with more unique
+#               values are more likely to be used as quasi-identifier.
 
-
-# TODO: Is there any personal Identifiable information in the data set?
-#       Is there any quasi-identifier information according to Sweeney's defination?
-#       Can you find possible quasi-identifier among the columns? 
-#       Why do you think they can be quasi-identifier?
 
 
 
 '''
 Question 2: Your supervisor believes the information in ['fnlwgt', 'education', 'relationship', 
             'capital_gain', 'capital_loss', 'hours_per_week'] are not necessary for your analysis.
-            Use suppression technique to de-idenitify this data set. How many rows are unique in 
-            the suppressed data set? 
-            Hint: Suppression technique and other de-identify techniques can be found in the reading 
-            list slides https://www2.cs.duke.edu/courses/fall12/compsci590.3/slides/lec3.pdf
+            Use suppression technique to de-idenitify this data set. Name the new data frame 
+            variable 'suppressed' and save it as 'suppressed.csv' without a header, without index. 
+            How many rows are unique in the 'suppressed' data set? 
 '''
 # TODO: Use suppression technique to de-idenitify this data set. 
-#       Then save the new data set under data directory as 'suppressed.csv' without a header.
-#       You can set header to False and index to False to save the data set without header and index
+#       Then save the new data set under data directory as 'suppressed.csv' without a header, without index.
+suppressed = 
 
 
-# TODO: How many rows in suppressed data set are unique?
-
+# TODO: How many rows in suppressed data set are unique? Name new dataframe as unique
+unique = 
 
 
 '''
-Question 3: Suppose now you have a sensitive data set of 10,000 rows named as "link_attack.csv",
+Question 3: Suppose now you have a sensitive dataframe of 10,000 rows named as "link_attack.csv",
             which contains personal identifiable information. Take a look at the head of this attack data,
-            perform a link attack and to re-identify people's name, ssn, race, and income. 
-            Hint: You can use ['age',  'sex, 'native_country'] as quasi-identifier. 
-            How many people can you identify?
+            perform a link attack and to re-identify people's name, ssn, race, and salary. You can use 
+            ['age',  'sex, 'native_country'] as quasi-identifier. How many people can you identify?
  
 '''
-# TODO: Perform link attack and identify individuals. How many of them can you idenity?
+link_attack = pd.read_csv('data/link_attack.csv')
+# TODO: your code here
+merged = 
 
+merged_unique = 
 
 '''
-Question 4: Consider the definition of k-anonymity, write a function to decide if 
+Question 4: Consider the definiaiton of k-anonymity, write a function to decide if 
             a data set is k-anonymous for a list of quasi-identifier. Then test if 
             this data set adult.csv is 2 anonymous with ['age', 'sex','native_country']
             being quasi-identifiers
+            Hint: method 1: use df.iterrows() to write for loop, or 
+                  method 2: use map() to sythesize columns to string and to compare strings.
+                  and there are more methods to explore...
 '''
 qsi = ['age', 'sex', 'native_country']
-
 def is_k_anonymous(k, df, qsi):
     '''
     k: a constant to descide if a data set is k anonymous
-    df: the data set of interest
+    df: the data frame of interest
     qsi: a list of quasi-identifier
     return: True if df is k-anonymous, False if not
     '''
-    # TODO: Your code here
+   # TODO: your code here
+   return True
 
 
 
@@ -77,21 +91,25 @@ def is_k_anonymous(k, df, qsi):
 Question 5: Now we wish to generalize the informations in adult.csv to make it k-anonymous while minimising
             the lose of information as little as possible. This problem turns out to be NP-Hard, which means 
             there is no polynomial algorithm to solve it. However, a compariably fast algorithm can solve it
-            sub-optimal. 
-            This algorithm is algready implemented for you. 
-            The detail of this algorithm can be found at: https://github.com/qiyuangong/Mondrian
+            sub-optimal. The detail of this algorithm can be found at: https://github.com/qiyuangong/Mondrian
             Read through it, and use this package to make this data set 2-anonymous.
 '''
+cmd = 'python3 anonymizer.py s a 2'
 
-# TODO: Use os module to run command 'python3 anonymizer.py s a 2'. Read the head generated data '
-#       data/anonymized.csv' then add column names ['age', 'workclass',  'education_num','marital_status',
-#       'moving', 'race', 'sex','native_country','income']
-#       back to the anonymized data set. Observe the data by printing out the head and describe funtion.
+# TODO: Use os module to run cmd command. 
 
+
+# TODO: Read the anonymized data generated by this Mondrian implementation and save it in anony variable.
+#        Then add column names ['age', 'workclass',  'education_num','marital_status', 'moving', 'race', 
+#       'sex','native_country','income'] and observe this data set.
+anony = 
 
 
 # TODO: Test if it is k-anonymous using the function in Question 3 and same quasi-identifier. 
-#       How many rows in this data set are unique now?
+#       How many rows in this data set are unique now? We can call the non-duplicate data set
+#       as unique anony.
+
+unique_anony = 
 
 
 
@@ -102,4 +120,45 @@ Question 6: Use the anonymized data set, analyze what percentage of male and fem
 
 
 
+def main():
+    # test 1
+    assert np.array_equal(df.columns, ['age', 'workclass', 
+            'fnlwgt', 'education', 'education_num', 'marital_status', 'moving', 'relationship', 
+            'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country','income']) == True
+    # test 2
+    assert np.array_equal(suppressed.columns, ['age', 'workclass', 'education_num',
+            'marital_status', 'moving', 'race', 'sex', 'native_country','income'] ) == True
 
+    assert unique.shape[0] == 17047
+
+    # test 3
+    assert merged_unique.shape[0] == 287
+
+    # test 4
+    assert is_k_anonymous(2, suppressed, qsi) == False
+
+    two_anon = pd.DataFrame(
+    [
+        ['*', 'Storm',34,'Black'],
+        ['John','*','*','*'],
+        ['*', 'Storm',34,'Black'],
+        ['John','*','*','*']
+    ],
+    columns = ['First','Last','age','race']
+)
+    assert is_k_anonymous(2, two_anon, ['First','Last','age','race']) == True
+
+    # test 5
+    assert is_k_anonymous(2,anony, qsi) == True
+    assert unique_anony.shape[0] == 0
+
+    # test 6
+    # print(df.groupby('sex').get_group(' Male')['income'].value_counts().tolist())
+    # print(df.groupby('sex').groups.keys())
+    # assert anony.groupby('sex').get_group('Male')['income'].value_counts().tolist()[0] == 13685
+    # assert df.groupby('sex').get_group(' Male')['income'].value_counts().tolist()[0] == 13685
+
+
+
+if __name__ == "__main__":
+    main()
